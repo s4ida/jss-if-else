3c6768ff87af0c1d5d70ac909dda54217bc4625e
s4ida <sd88c9gvh@code.edu.az>
2023-10-20 00:37:55 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+let a = 30
+let b = 27 
+if (a>b) {
+    console.log('a b-dən böyükdür.');
+}
+else if  (a=b){
+    console.log('a b-yə bərabərdir.');
+}
+else {
+    console.log('a b-dən kiçikdir.');
+}
